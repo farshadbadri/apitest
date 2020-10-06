fdfdc9ff256fe43e50b402c61d649b78b477ab35
farshadbadri <72414264+farshadbadri@users.noreply.github.com>
2020-10-06 14:15:24 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+
+<h1> something most be tested in my gihub  </h1>
+<p> sdsdsdsdsdsd</p></br>
+<p> sdsdsdsdsdsd</p>
+<body>
+
+</body>
+</html>
+
+
+<?php 
+ 
+echo  "https://api.telegram.org/".$bot_token."/getMe";
+
+include 'madeline.php';
+
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+
+$Updates = $MadelineProto->channels->inviteToChannel(['channel' => InputChannel, 'users' => [InputUser, InputUser], ]);
+
+
+?>
