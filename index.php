ceb53e002c993207a6e0756f9c118c58c0aab3f4
farshadbadri <72414264+farshadbadri@users.noreply.github.com>
2020-10-06 08:51:51 +0330

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,17 @@
 
 </body>
 </html>
+
+
+<?php 
+$bot_token = "1271578252:AAEQukeL4N8n0NCLAjc-w0MifrIXHb7dBI8";
+echo  "https://api.telegram.org/".$bot_token."/getMe";
+// include 'madeline.php';
+
+// $MadelineProto = new \danog\MadelineProto\API('session.madeline');
+// $MadelineProto->start();
+
+// $Updates = $MadelineProto->channels->inviteToChannel(['channel' => InputChannel, 'users' => [InputUser, InputUser], ]);
+
+
+?>
